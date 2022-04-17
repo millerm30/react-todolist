054c10aa993de3b35cafab8e9bd8d69c810d0e64
millerm30 <millergm30@gmail.com>
2022-04-17 13:20:26 -0230

    test notes

---
+++ src/components/TodoHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoHeader = () => {
+  return (
+    <div className='headerContent'>
+        <h1>ToDo List</h1>
+    </div>
+  )
+}
+
+export default TodoHeader
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import TodoContainer from './components/TodoContainer'
 
 const createElement = document.querySelector('#root')
 const root = createRoot(createElement)
 
 root.render(
   <StrictMode>
-    
+    <TodoContainer />
   </StrictMode>
 )
